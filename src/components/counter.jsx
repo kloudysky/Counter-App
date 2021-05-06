70dd71c22f143643c21666fa4205ebaccfd20d4f
Adam Kamboj <kamboj.adam@gmail.com>
2021-05-05 20:26:03 -0400

    added navbar and lifted state from counter to app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
   render() {
+    const counter = this.props.counter;
     return (
       <div>
+        {console.log(counter.id)}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
   }
 
-  handleIncrement = () => {
-    console.log(this.state.count);
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "zero" : value;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
